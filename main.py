f4fd035c2f12288da24537d727330fb26abedc92
gabriel-wer <gabrielflpa@gmail.com>
2023-02-12 18:38:46 -0300

    clean up and fixed canvas sizing

--- main.py
+++ main.py
@@ -3,17 +3,16 @@
 import random 
 import pygame
 
-SIZE_X = 20
-SIZE_Y = 20
-ROWS = 20
-COLUMNS = 20
+CELLSIZE = 20
+ROWS = 30
+COLUMNS = 30
 
 def draw_map(arr, canvas):
     for i, _ in enumerate(arr):
         for j, _ in enumerate(arr[i]):
             if arr[i][j] == 1:
                 pygame.draw.rect(canvas, (0,255,0),
-                                 pygame.Rect(j*ROWS, i*COLUMNS, SIZE_X, SIZE_Y))
+                                 pygame.Rect(i*CELLSIZE, j*CELLSIZE, CELLSIZE-2, CELLSIZE-2))
 
 def calculate(neigh, c):
     if neigh < 2:
@@ -41,52 +40,26 @@ def detect_collision(arr, i, j, c):
         count -= 1
     return calculate(count, c)
 
-
-
-
 pygame.init()
-canvas = pygame.display.set_mode((ROWS*SIZE_X, COLUMNS*SIZE_Y))
+canvas = pygame.display.set_mode(((CELLSIZE *  COLUMNS), (CELLSIZE * ROWS)))
 pygame.display.set_caption("Conway's Game of Life")
 
 #populates array with values from 0-1
 arr = [[random.randint(0,1) for _ in range(ROWS)] for _ in range(COLUMNS)]
-
+print(arr)
 
 clock = pygame.time.Clock()
 running = True
 while running:
-    draw_map(arr, canvas)
     canvas.fill((0,0,0))
     for i, _ in enumerate(arr):
         for j, c in enumerate(arr[i]):
             arr[i][j] = detect_collision(arr, i, j, c)
             draw_map(arr, canvas)
-            pygame.display.update()
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-
-"""
-[[0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1],
- [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1],
- [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1],
- [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1],
- [0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0],
- [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1],
- [1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
- [0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
- [0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0],
- [0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1],
- [0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1],
- [1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0],
- [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
- [1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0],
- [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
- [0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
- [1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0],
- [1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1],
- [1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0],
- [0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0]]
-
-"""
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False 
